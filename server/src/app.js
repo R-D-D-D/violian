aa14cf191900c8f484bfea25b07b62f2d233b403
R-D-D-D <45935526+R-D-D-D@users.noreply.github.com>
2020-06-03 18:41:08 +0800

    setting up server and client

---
+++ server/src/app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const morgan = require('morgan')
+
+const app = express()
+app.use(morgan('combined'))
+app.use(bodyParser.json())
+app.use(cors())
+
+app.listen(process.env.PORT || 8081)
+
+app.get('/status', (req, res) => {
+  res.send({
+    message: 'hello world'
+  })
+})
+
+console.log('hello world')
