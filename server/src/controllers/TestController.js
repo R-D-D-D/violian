3f4127aaf9b991469e801289cf617d3386c387be
R-D-D-D <wangrunding@gmail.com>
2020-08-09 02:22:33 +0800

    first v of ffmpeg and shaka packager

--- server/src/controllers/TestController.js
+++ server/src/controllers/TestController.js
@@ -2,6 +2,7 @@ const AWS = require('aws-sdk')
 const config = require('../config/config')
 const ffmpeg = require('../lib/ffmpeg')
 const fs = require('fs')
+const path = require('path')
 
 const s3 = new AWS.S3({
   accessKeyId: config.aws.id,
@@ -12,12 +13,27 @@ module.exports = {
   async create (req, res) {
     try {
       if (req.file) {
-        fs.writeFileSync(`/home/runding/Documents/${req.file.originalname}`, req.file.buffer);
-      } else {
-        fs.writeFileSync(`/home/runding/Documents/test.txt`, 'Hey yo');
-      }
+        var srcDir = '/Users/rundingwang/Documents/Github/node-learning/server/src/scripts/src/';
+        var destDir = '/Users/rundingwang/Documents/Github/node-learning/server/src/scripts/dest/';
 
-      await ffmpeg.changeVideoResolution(`/home/runding/Documents/${req.file.originalname}`, '640x360')
+        if (!fs.existsSync(srcDir)){
+            fs.mkdirSync(srcDir);
+        }
+
+        fs.writeFileSync(srcDir + req.file.originalname, req.file.buffer);
+        let metadata = await ffmpeg.getMetaData(srcDir + req.file.originalname)
+        console.log(metadata)
+        let videoHeight = metadata.streams[0].height
+        let videoAspectRatio = metadata.streams[0].display_aspect_ratio
+
+        // await ffmpeg.createVideoWithResolution(srcDir + req.file.originalname, '1280x720')
+        await ffmpeg.createVideoWithResolution(srcDir + req.file.originalname, '640x360')
+        await ffmpeg.createAudio(srcDir + req.file.originalname)
+        // await ffmpeg.createDashManifest(srcDir + req.file.originalname)
+
+        // fs.rmdirSync(srcDir)
+        // fs.rmdirSync(destDir)
+      }
 
       res.send({
         data: 'ok'
--- server/src/lib/ffmpeg.js
+++ server/src/lib/ffmpeg.js
@@ -1,20 +1,33 @@
 const ffmpeg = require('fluent-ffmpeg')
 const fs = require('fs')
 const path = require('path')
-
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 
 module.exports = {
-
-  async changeVideoResolution (pathToVid, resolution) {
+  async createVideoWithResolution (pathToVid, resolution) {
     const pathObj = path.parse(pathToVid);
-    const bitrate = 10;
-    const promise = new Promise((resolve, reject) => {
+    // let bitrate = '1500k';
+    let finalRes = resolution
+    // switch (resolution) {
+    //   case '1280x720':
+    //     bitrate = '1500k'
+    //     break
+    //   case '854x480':
+    //     bitrate = '750k'
+    //     finalRes = '640x?'
+    //     break
+    // }
+
+    await new Promise((resolve, reject) => {
       ffmpeg(pathToVid)
         .noAudio()
-        .size(resolution)
+        // .aspect('16:9')
+        .size(finalRes)
+        .autopad()
         .toFormat('mp4')
         .videoCodec('libx264')
-        .output(`${pathObj.dir}/${pathObj.name}_${resolution.split('x')[1]}${pathObj.ext}`)
+        .output(`${pathObj.dir}/${pathObj.name}${resolution.split('x')[1]}_video.mp4`)
         .on('progress', (progress) => {
           console.log(progress)
         })
@@ -28,11 +41,55 @@ module.exports = {
         })
         .run()
     });
-
-    await promise
   },
 
-  getAudio (pathToVid) {
-    var command = ffmpeg('/path/to/file.avi').audioBitrate('128k').noVideo()
+  async createAudio (pathToVid) {
+    const pathObj = path.parse(pathToVid)
+    await new Promise((resolve, reject) => {
+      ffmpeg(pathToVid)
+        .noVideo()
+        .toFormat('mp4')
+        .output(`${pathObj.dir}/${pathObj.name}_audio.mp4`)
+        .on('progress', (progress) => {
+          console.log(progress)
+        })
+        .on('error', (err) => {
+          console.log(`[ffmpeg] error: ${err.message}`)
+          reject(err);
+        })
+        .on('end', () => {
+          console.log('[ffmpeg] finished')
+          resolve();
+        })
+        .run()
+    });
+  },
+
+  async createDashManifest (pathToVid) {
+    const { stdout, stderr } = await exec(`packager \
+      input=src/scripts/src/sample720_video.mp4,stream=video,output=dest/sample720_video.mp4 \
+      input=src/scripts/src/sample360_video.mp4,stream=video,output=dest/sample480_video.mp4 \
+      input=src/scripts/src/sample_audio.mp4,stream=audio,output=dest/sample_audio.mp4 \
+      --profile on-demand \
+      --mpd_output dest/sample-manifest-full.mpd \
+      --min_buffer_time 3 \
+      --segment_duration 3`);
+    console.log('stdout:', stdout);
+    console.error('stderr:', stderr);
+  },
+
+  async getMetaData (pathToVid) {
+    let metadata = {}
+    await new Promise((resolve, reject) => {
+      ffmpeg.ffprobe(pathToVid, function(err, meta) {
+        if (err) {
+          reject(err)
+        } else {
+          metadata = meta
+          resolve()
+        }
+      });
+    });
+    return metadata
   }
 }
